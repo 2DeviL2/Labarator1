2a462e7bd4b17f916c7dda6fd9c7324657fefc40
2DeviL2 <102857400+2DeviL2@users.noreply.github.com>
2022-06-01 21:05:45 +0400

    Добавьте файлы проекта.

---
+++ WebApplication8/WebForm1.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace WebApplication8
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SubmitPasswordLogin_Click(object sender, EventArgs e)
+        {
+
+            var loginPassword = new Dictionary<string, string>()
+            {
+                { "oleg", "123"},
+                { "anton", "345"},
+                { "peter", "zxc"}
+            };
+
+            if (loginPassword.ContainsKey(textLogin.Text.ToLower()))
+            {
+                if (loginPassword[textLogin.Text.ToLower()] == textPassword.Text)
+                {
+                    Response.Redirect("welcome.aspx");
+                }
+                else wrongLoginOrPassword.Visible = true;
+
+            }
+            else wrongLoginOrPassword.Visible = true;
+
+            textLogin.Text = "";
+            textPassword.Text = "";
+        }
+
+        protected void SubmitData_Click(object sender, EventArgs e)
+        {
+            if (text.Text.Length == 0
+                || list.SelectedIndex == -1
+                || radioButtonList.SelectedIndex == -1)
+            {
+                labeDataClear.Text = "Вы не ввели все данные";
+                labeDataClear.Visible = true;
+                return;
+            }
+
+            labelData.Visible = true;
+            labelData.Text = text.Text
+                + list.SelectedItem.ToString()
+                + dropDownList.SelectedItem.ToString()
+                + radioButtonList.SelectedItem.ToString()
+            ;
+
+        }
+
+    }
+}
---
+++ WebApplication8/welcome.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="welcome.aspx.cs" Inherits="WebApplication8.welcome" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="welcomePage" runat="server" Text="Добро пожаловать!!!"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
